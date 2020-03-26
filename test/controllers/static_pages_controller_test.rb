226a9d350413502a46a42d9a88382b5e744bf73a
adbeje <adria.venturaherce@gmail.com>
2020-03-26 16:06:21 +0000

    Pagina estatica con estilo

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get news" do
     get static_pages_news_url
     assert_response :success
-    assert_select "title", "ProjectAswHackersnews"
+    assert_select "title", "HackersNewsClone"
   end
 
 end
