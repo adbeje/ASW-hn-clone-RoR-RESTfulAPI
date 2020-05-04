4dddc1b70bd7a36a70da0e42d023b4cbe9469568
Ubuntu <ubuntu@ip-172-31-81-6.ec2.internal>
2020-05-04 16:31:10 +0000

    api branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: "users/registrations" }
   resources :users, :only => [:show]
 
+scope "/api",defaults: {format: 'json'} do
+  
+  get '/contribucions' =>'api/contribucions#index'
+  
+end
+
   resources :contribucions do
     member do
       get "like"
